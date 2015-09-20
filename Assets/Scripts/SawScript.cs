0bbf18e114dec324da5e3c827dfaa0ba51b5600d
George Alexandria <giorgi.alexandria@gmail.com>
2015-09-20 20:11:28 +0300

    Added rotation saw

---
+++ Assets/Scripts/SawScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SawScript : MonoBehaviour
+{
+    public float SpeedRotation = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, SpeedRotation));
+    }
+}
