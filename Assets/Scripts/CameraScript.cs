4369fbafd86d54aee661db5a9dd8dd18bb56a5b2
George Alexandria <giorgi.alexandria@gmail.com>
2015-09-21 00:09:36 +0300

    Added event handler by collider, move camera by hero

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform target;
+    private Vector3 delta;
+
+    // Use this for initialization
+    void Start()
+    {
+        delta = Camera.main.transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            Camera.main.transform.position = target.position + delta;
+        }
+    }
+}
---
+++ Assets/Scripts/HeroScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroScript : MonoBehaviour
+{
+    public float Speed = 0.02f;
+    public float JumpForce = 1500f;
+    private Vector3 spawn;
+
+    // Use this for initialization
+    void Start()
+    {
+        spawn = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(transform.position.x + Speed, transform.position.y, transform.position.z);
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, JumpForce));
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "die")
+        {
+            transform.position = spawn;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
