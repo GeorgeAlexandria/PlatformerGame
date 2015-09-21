68cfba96a7dba7fc137b9d504a5514ce11619c3b
George Alexandria <giorgi.alexandria@gmail.com>
2015-09-21 10:59:52 +0300

    Added representation of hearts

--- Assets/Scripts/HeroScript.cs
+++ Assets/Scripts/HeroScript.cs
@@ -5,7 +5,12 @@ public class HeroScript : MonoBehaviour
 {
     public float Speed = 0.02f;
     public float JumpForce = 1500f;
+    public Texture Heart;
+    public float Scaleheart = 0.3f;
+
     private Vector3 spawn;
+    private readonly Vector2 positionHearts = new Vector2(5, 5);
+    private int countHearts = 3;
 
     // Use this for initialization
     void Start()
@@ -24,9 +29,20 @@ public class HeroScript : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.tag == "die")
+        IsDie(collision.collider);
+    }
+
+    void OnTriggerEnter2d(Collider2D collider)
+    {
+        IsDie(collider);
+    }
+
+    void IsDie(Collider2D collider)
+    {
+        if (collider.tag == "die")
         {
             transform.position = spawn;
+            countHearts--;
         }
     }
 
@@ -34,5 +50,15 @@ public class HeroScript : MonoBehaviour
     void Update()
     {
 
+    }
+
+    void OnGUI()
+    {
+        float height = Heart.height * Scaleheart;
+        float width = Heart.width * Scaleheart;
+        for (int i = 0; i < countHearts; i++)
+        {
+            GUI.Label(new Rect(positionHearts.x + i * height, positionHearts.y, height, width), Heart);
+        }
     }
 }
