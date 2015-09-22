7c0283d8a468bc1630446bc46db0b0e9bd9d8b08
George Alexandria <giorgi.alexandria@gmail.com>
2015-09-22 23:56:57 +0300

    Added hero's jump

--- Assets/Scripts/HeroScript.cs
+++ Assets/Scripts/HeroScript.cs
@@ -3,28 +3,33 @@ using System.Collections;
 
 public class HeroScript : MonoBehaviour
 {
-    public float Speed = 0.02f;
-    public float JumpForce = 1500f;
+    public float Speed = 0.04f;
+    public float JumpForce = 13f;
     public Texture Heart;
+    public LayerMask whatIsGround;
+    public Transform GroundCheck;
     public float Scaleheart = 0.3f;
 
     private Vector3 spawn;
     private readonly Vector2 positionHearts = new Vector2(5, 5);
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+
     private int countHearts = 3;
+    private bool isGround;
+    private float groundRadius = 0.02f;
 
     // Use this for initialization
     void Start()
     {
         spawn = transform.position;
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
-        transform.position = new Vector3(transform.position.x + Speed, transform.position.y, transform.position.z);
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, JumpForce));
-        }
+        isGround = Physics2D.OverlapCircle(GroundCheck.position, groundRadius, whatIsGround);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -49,7 +54,11 @@ public class HeroScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        transform.position = new Vector3(transform.position.x + Speed, transform.position.y, transform.position.z);
+        if (isGround && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rigidBody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
     }
 
     void OnGUI()
