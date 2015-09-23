03c4e86f043dd9ee128ec4a0fa415527e5343389
George Alexandria <giorgi.alexandria@gmail.com>
2015-09-24 01:53:54 +0300

    Added transitions between animations

--- Assets/Scripts/HeroScript.cs
+++ Assets/Scripts/HeroScript.cs
@@ -14,9 +14,11 @@ public class HeroScript : MonoBehaviour
     private readonly Vector2 positionHearts = new Vector2(5, 5);
     private Rigidbody2D rigidBody;
     private Animator animator;
+    //private Animation animation;
 
     private int countHearts = 3;
     private bool isGround;
+    private bool isJump;
     private float groundRadius = 0.02f;
 
     // Use this for initialization
@@ -34,20 +36,32 @@ public class HeroScript : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.collider.tag == "Ground")
+        {
+            isJump = false;
+        }
         IsDie(collision.collider);
     }
 
-    void OnTriggerEnter2d(Collider2D collider)
+    void OnTriggerEnter2D(Collider2D collider)
     {
+        if (collider.tag == "Heart")
+        {
+            Destroy(collider.gameObject);
+            countHearts += countHearts < 3 ? 1 : 0;
+            return;
+        }
         IsDie(collider);
     }
 
     void IsDie(Collider2D collider)
     {
-        if (collider.tag == "die")
+        if (collider.tag == "Die")
         {
             transform.position = spawn;
             countHearts--;
+            isGround = true;
+            isJump = false;
         }
     }
 
@@ -55,9 +69,22 @@ public class HeroScript : MonoBehaviour
     void Update()
     {
         transform.position = new Vector3(transform.position.x + Speed, transform.position.y, transform.position.z);
+        if (!isGround && !isJump)
+        {
+            animator.CrossFade("Fall", 0f);
+            return;
+        }
         if (isGround && Input.GetKeyDown(KeyCode.UpArrow))
         {
+            isJump = true;
+            isGround = false;
             rigidBody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            animator.CrossFade("Jump", 0f);
+            return;
+        }
+        if (isGround && !isJump)
+        {
+            animator.CrossFade("Run", 0f);
         }
     }
 
