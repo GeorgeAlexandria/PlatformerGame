641b80119d2814bdb85f28124bf730a533817dfb
George Alexandria <giorgi.alexandria@gmail.com>
2015-09-26 03:15:08 +0300

    Smooth Following camera

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,23 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraScript : MonoBehaviour {
-
-    public Transform target;
+public class CameraScript : MonoBehaviour
+{
+    public Transform Target;
+    private const float maxDistance = 3.0f;
     private Vector3 delta;
+    private const int positionZ = -10;
 
     // Use this for initialization
     void Start()
     {
-        delta = Camera.main.transform.position - target.position;
+        delta = Target.position - Camera.main.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (target)
-        {
-            Camera.main.transform.position = target.position + delta;
-        }
+        Vector3 temp = Vector3.Lerp(transform.position, Target.position - delta, Time.deltaTime * maxDistance);
+        temp.z = positionZ;
+        Camera.main.transform.position = temp;
     }
 }
