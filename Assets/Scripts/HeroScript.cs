5cdf6fcb2eb4e3ee7ec973a5605d501f9cf1f4ef
George Alexandria <giorgi.alexandria@gmail.com>
2015-10-03 19:46:01 +0300

    Resolve problem with approximately collision detecion

--- Assets/Scripts/HeroScript.cs
+++ Assets/Scripts/HeroScript.cs
@@ -1,104 +1,154 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class HeroScript : MonoBehaviour
 {
+    #region PublicVariables
     public float Speed = 0.04f;
     public float JumpForce = 13f;
     public Texture Heart;
     public LayerMask whatIsGround;
     public Collider2D GroundCheck;
     public float Scaleheart = 0.3f;
+
     public PolygonCollider2D RunCollider;
     public PolygonCollider2D JumpCollider;
     public PolygonCollider2D FallCollider;
+    #endregion
 
+    #region PrivateVariables
     private Vector3 spawn;
     private readonly Vector2 positionHearts = new Vector2(5, 5);
     private Rigidbody2D rigidBody;
     private Animator animator;
-    //private int defaultLayer;
-
     private int countHearts = 3;
     private const float groundRadius = 0.2f;
+    #endregion
 
-    private bool isGround = true;
+    #region State
+    private bool isGrounded = true;
+    private bool isPreviouslyGrounded;
     private bool isJump;
+    private bool isJumping;
+    #endregion
 
+    #region NowNotUse
     private const string run = "Run";
     private const string jump = "Jump";
     private const string fall = "Fall";
+    #endregion
 
+    #region CollisionDetectionVariables
     private Vector2 oldPosition;
     private Vector2 newPosition;
+    private int count;
+    private float epsilon = 0.001f;
+
+    private float normalGravity = 2f;
+    private float extendedGravity = 20f;
+    private int countRepeat = 10;
+    #endregion
 
     // Use this for initialization
     void Start()
     {
-        //oldPosition = rigidBody.position;
-        //newPosition = rigidBody.position - new Vector2(1, 1);
         rigidBody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-
         spawn = rigidBody.position;
+    }
 
-        //defaultLayer = LayerMask.GetMask("Default");
+    bool CheckCount(int arg)
+    {
+        if (count++ >= arg)
+        {
+            count = 0;
+            return true;
+        }
+        return false;
     }
 
     void FixedUpdate()
     {
-        isGround = Physics2D.OverlapCircle(GroundCheck.transform.position, groundRadius, whatIsGround);
-    }
+        newPosition = rigidBody.position;
+        rigidBody.gravityScale = normalGravity;
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.collider.tag == "Ground" && Physics2D.IsTouching(collision.collider, GroundCheck) ||
-            Physics2D.OverlapCircle(GroundCheck.transform.position, groundRadius, whatIsGround))
+        IsGrounded();
+        if (isGrounded)
         {
-            Grounded();
+            if (isJump)
+            {
+                rigidBody.AddForce(new Vector2(Speed, JumpForce), ForceMode2D.Impulse);
+                isJumping = true;
+            }
+            else if (isPreviouslyGrounded && isJumping)
+            {
+                //Just jump, but caught in infinity jumping
+                count = Mathf.Abs(oldPosition.x - newPosition.x) < epsilon && Mathf.Abs(oldPosition.y - newPosition.y) < epsilon ? count + 1 : 0;
+                rigidBody.gravityScale = CheckCount(countRepeat) ? extendedGravity : rigidBody.gravityScale;
+            }
         }
-        if (collision.collider.tag == "Die")
+        else
         {
-            Died();
+            if (!isPreviouslyGrounded)
+            {
+                //Infinity falling if !isJumping else infinity jumping
+                count = Mathf.Abs(oldPosition.x - newPosition.x) < epsilon && Mathf.Abs(oldPosition.y - newPosition.y) < epsilon ? count + 1 : 0;
+                rigidBody.gravityScale = CheckCount(countRepeat) ? extendedGravity : rigidBody.gravityScale;
+            }
         }
-    }
+        rigidBody.velocity = new Vector2(Speed, rigidBody.velocity.y);
 
-    //Надо подправить
-    void Died()
-    {
-        transform.position = spawn;
-        countHearts--;
-        Grounded();
-    }
-
-    void Jumped()
-    {
-        isJump = true;
-        isGround = false;
-    }
-
-    void Grounded()
-    {
         isJump = false;
-        isGround = true;
+        oldPosition = rigidBody.position;
     }
 
-    void OnTriggerEnter2D(Collider2D collider)
+    // Update is called once per frame
+    void Update()
     {
-        switch (collider.tag)
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            case "Heart":
-                Destroy(collider.gameObject);
-                countHearts += countHearts < 3 ? 1 : 0;
-                break;
-            case "Die":
-                Died();
-                break;
-            default:
-                break;
+            isJump = true;
+        }
+        if (!isGrounded && !isJumping)
+        {
+            //ActivateCollider(fall);
+            animator.CrossFade(fall, 0f);
+        }
+        else if (isJumping)
+        {
+            //ActivateCollider(jump);
+            animator.CrossFade(jump, 0f);
+        }
+        else if (isGrounded)
+        {
+            //ActivateCollider(run);
+            animator.CrossFade(run, 0f);
         }
     }
 
+    private void IsGrounded()
+    {
+        isPreviouslyGrounded = isGrounded;
+        isGrounded = false;
+
+        //isGrounded = Physics2D.IsTouchingLayers(GroundCheck, whatIsGround);
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(GroundCheck.transform.position, groundRadius, whatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                isGrounded = true;
+                break;
+            }
+        }
+        if (!isPreviouslyGrounded && isGrounded)
+        {
+            isJumping = false;
+        }
+    }
+
+    #region NotUse
     void ActivateCollider(string arg)
     {
         RunCollider.enabled = false;
@@ -117,43 +167,51 @@ public class HeroScript : MonoBehaviour
                 break;
         }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void StickToGroundHelper()
     {
-        oldPosition = rigidBody.position;
-        if (!isGround && !isJump)
+        RaycastHit hitInfo;
+        if (Physics.SphereCast(transform.position, groundRadius, Vector3.down, out hitInfo, 1f, whatIsGround))
         {
-            ActivateCollider(fall);
-            animator.CrossFade(fall, 0f);
-            //State of infinity faling
-            //But it's not work
-            if (Mathf.Abs(newPosition.x - oldPosition.x) < 0.00001 && Mathf.Abs(newPosition.y - oldPosition.y) < 0.00001)
+            if (Mathf.Abs(Vector3.Angle(hitInfo.normal, Vector3.up)) < 85f)
             {
-                rigidBody.AddForce(new Vector2(0.1f, 0.1f), ForceMode2D.Impulse);
+                rigidBody.velocity = Vector3.ProjectOnPlane(rigidBody.velocity, hitInfo.normal);
             }
         }
-        else if (isGround && Input.GetKeyDown(KeyCode.UpArrow))
+    }
+    #endregion
+
+    //Need modify
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        IsGrounded();
+        if (collision.collider.tag == "Die")
         {
-            Jumped();
-            rigidBody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
-            ActivateCollider(jump);
-            animator.CrossFade(jump, 0f);
-            //return;
+            Died();
         }
-        else if (isGround && !isJump)
+    }
+
+    //Need modify
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        switch (collider.tag)
         {
-            ActivateCollider(run);
-            animator.CrossFade(run, 0f);
+            case "Heart":
+                Destroy(collider.gameObject);
+                countHearts += countHearts < 3 ? 1 : 0;
+                break;
+            case "Die":
+                Died();
+                break;
+            default:
+                break;
         }
-        //State of infinity jumping
-        else if (!isGround && isJump &&
-            Mathf.Abs(newPosition.x - oldPosition.x) < 0.00001 && Mathf.Abs(newPosition.y - oldPosition.y) < 0.00001)
-        {
-            rigidBody.AddForce(new Vector2(0.1f, 0.1f), ForceMode2D.Impulse);
-        }
-        rigidBody.velocity = new Vector2(Speed, rigidBody.velocity.y);
-        newPosition = rigidBody.position;
+    }
+
+    //Need modify
+    void Died()
+    {
+        transform.position = spawn;
+        countHearts--;
     }
 
     void OnGUI()
