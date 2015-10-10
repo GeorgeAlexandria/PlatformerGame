019a15c67c977c677ea277716c830d0e43dae05c
George Alexandria <giorgi.alexandria@gmail.com>
2015-10-11 01:00:44 +0300

    Added new management interfaces

---
+++ Assets/Scripts/ApplicationManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplicationManager : MonoBehaviour
+{
+    public IRestarterLevel restarter;
+    public IFinalizerApplication finalizer;
+    public IStarterApplication starter;
+    public IContinuerApplication continuer;
+    public IBreakerApplication breaker;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Interfaces/IBreakerApplication.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IBreakerApplication
+{
+    void Pause();
+}
